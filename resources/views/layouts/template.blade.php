57eee76a163154b318fa6c9452c371e4e40886e0
maremagnum01 <eze.di.lallo@gmail.com>
2025-03-02 19:30:47 -0300

    se agregaron pequeños cambios

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>iStore D | iPhone nuevos y usados</title>
         <!-- Fonts -->
+        <link rel="icon" href="imagenes/tienda-online.png" type="image/png">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />      
